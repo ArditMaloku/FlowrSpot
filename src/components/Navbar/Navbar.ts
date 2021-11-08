2e12469661c2ce944e132718d17161952670170e
ardit-maloku <71756999+ardit-maloku@users.noreply.github.com>
2021-11-08 22:43:05 +0100

    Added menu for mobile screen

--- src/components/Navbar/Navbar.ts
+++ src/components/Navbar/Navbar.ts
@@ -1,4 +1,4 @@
-import { defineComponent } from '@vue/runtime-core';
+import { defineComponent, onMounted, watch } from '@vue/runtime-core';
 import LoginComponent from '../Login/index.vue';
 import UserInfoComponent from '../UserInfo/index.vue';
 import SignupComponent from '../Signup/index.vue';
@@ -12,11 +12,30 @@ export default defineComponent({
         UserInfoComponent,
     },
     setup() {
-        const isDesktop = window.innerWidth > 767;
         const showMenu = ref(false);
         const loginModalVisible = ref(false);
 
-        if (isDesktop) showMenu.value = true;
+        onMounted(() => {
+            if (window.innerWidth > 767) {
+                showMenu.value = true;
+            }
+
+            window.addEventListener('resize', function () {
+                if (window.innerWidth > 767) {
+                    showMenu.value = true;
+                } else {
+                    showMenu.value = false;
+                }
+            });
+        });
+
+        watch(showMenu, (currentValue) => {
+            if (currentValue) {
+                document.documentElement.style.overflow = 'hidden';
+            } else {
+                document.documentElement.style.overflow = 'auto';
+            }
+        });
 
         return { showMenu, loginModalVisible };
     },
